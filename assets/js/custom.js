5d3276e711b234042556f7c915e80cf53e6b0d82
Scot Campbell <sr4001@gmail.com>
2024-10-17 22:28:15 -0400

    slowly working through lighthouse improvements

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,9 @@
 // Put your custom JS code here
+import 'lazysizes'
+
+// Fallback for browsers that don't support native lazy loading
+if (!('loading' in HTMLImageElement.prototype)) {
+    const script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+    document.body.appendChild(script);
+  }
