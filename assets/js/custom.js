bf8509a2d0eb34c9a43d305f34a459dde11850d1
Scot Campbell <sr4001@gmail.com>
2024-10-17 21:22:42 -0400

    updated a few things (css, scripting, package.json)

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,1 @@
 // Put your custom JS code here
-
-// Fallback for browsers that don't support native lazy loading
-if (!('loading' in HTMLImageElement.prototype)) {
-  const script = document.createElement('script');
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
-  document.body.appendChild(script);
-}
